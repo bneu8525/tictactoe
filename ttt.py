c7d85a997182481d091587bb6d2f404be52ff3ad
bneu8525 <bneu8525@student.rcschools.net>
2019-04-09 11:27:23 -0500

    a

--- ttt.py
+++ ttt.py
@@ -1,1 +1,20 @@
+from tkinter import *
+
+
+class TTT():
+    def __init__(self, master):
+        self.mymaster = master
+
+
+
+def main():
+    root = Tk()
+    TTT(root)
+    root.mainloop()
+
+main()
+
+
+
+
 
